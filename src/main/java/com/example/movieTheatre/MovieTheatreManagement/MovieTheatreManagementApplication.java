4d540c5cda1806ac9478f9628ca846f91155877c
siddhantkharode <kharodesiddhant32@gmail.com>
2022-01-26 22:00:55 +0530

    initial commit

---
+++ src/main/java/com/example/movieTheatre/MovieTheatreManagement/MovieTheatreManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.movieTheatre.MovieTheatreManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieTheatreManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieTheatreManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/movieTheatre/MovieTheatreManagement/MovieTheatreManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.movieTheatre.MovieTheatreManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieTheatreManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
